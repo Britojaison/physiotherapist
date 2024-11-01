d34d4dde5bd266335368e6df0fb5a9b931e12d88
Britojaison <britojaison123@gmail.com>
2024-11-01 14:47:52 +0530

    done.

--- src/components/FrequencySettings.jsx
+++ src/components/FrequencySettings.jsx
@@ -4,7 +4,6 @@ function FrequencySettings() {
   return (
     <div>
       <label>Days of the Week:</label>
-      {/* Checkboxes for each day */}
       <label>Sessions per Day:</label>
       <input type="number" min="1" max="5" />
     </div>
